d80a48037c6c3422d1be65dc38010efc04a2fbb6
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2018-08-12 18:15:28 +0900

    レモンとトマトの数を一つずつにした

--- main.rb
+++ main.rb
@@ -6,25 +6,17 @@ require_relative 'tomato'
 Window.width = 512
 Window.height = Window.width / 4 * 3
 background = Image.load('images/green.png')
-
-objects = []
-objects << Player.new
-img = Image.load('images/lemon.png')
-# objects << Enemy.new(rand(1024), rand(768), img)
-objects << Enemy.new(430, rand(430*3/4), img)
-#spaceの回数.times do~~
-# tomato << Tomato.new#ここのトマト新規作成times?
-tomato_img = Image.load('images/tomato.png')
-# tomato = []
-
-tmt = Tomato.new(tomato_img)
+man = Player.new
+img = Image.load('images/lemon.png') 
+enemy = Enemy.new(430, rand(430*3/4), img)
+tmt = Tomato.new(Image.load('images/tomato.png'))
 Window.loop do
   Window.draw(0,0,background)
   break if Input.key_push?(K_ESCAPE)
-  objects.each do |obj|
-    obj.move
-    obj.draw
-  end
+  man.move
+  man.draw
+  enemy.move
+  enemy.draw
 
   if Input.key_push?(K_SPACE)
     tmt.nage
